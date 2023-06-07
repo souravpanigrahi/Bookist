f4365d64b58508887fd05304a16500912b155dfa
souravpanigrahi <souravpanigrahi2021@gmail.com>
2023-06-07 22:55:45 +0530

    Children Props

--- src/index.js
+++ src/index.js
@@ -5,24 +5,48 @@ const author = "Emily Henry";
 const title = "Happy Place";
 const img = "./images/71+vQyk44IL.jpg";
 
+const firstBook = {
+  author: "Emily Henry",
+  title: "Happy Place",
+  img: "./images/71+vQyk44IL.jpg",
+};
+const secondBook = {
+  author: "Darius Foroux",
+  title: "Do It Today",
+  img: "https://m.media-amazon.com/images/I/61ZPDQOjw-L.jpg",
+};
 
-const Bookist = () => {
+function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer"/>
-      <Book title="random title" number={22}/>
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
+        </p>
+        <button>click me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
-};
-const Book = (props) => {
-  console.log(props);
+}
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
-      <img src={img} alt="{title}" />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author} </h4>
+      {children}
     </article>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Bookist></Bookist>);
+root.render(<BookList></BookList>);
